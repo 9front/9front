5a333eb240a9e3622c2f443310a7c1856a6551e8
cinap_lenrek <cinap_lenrek@centraldogma>
2011-06-02 06:52:29 +0000

    games/doom: remove debug abort()

--- sys/src/games/doom/v_video.c
+++ sys/src/games/doom/v_video.c
@@ -228,7 +228,6 @@ V_DrawPatch
       fprintf( stderr, "Patch at %d,%d exceeds LFB\n", x,y );
       // No I_Error abort - what is up with TNT.WAD?
       fprintf( stderr, "V_DrawPatch: bad patch (ignored)\n");
-abort();
       return;
     }
 #endif 
