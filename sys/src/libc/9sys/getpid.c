23fcac090ca7e6d0856572f107ca6b2ade0ada5b
aiju <aiju@phicode.de>
2011-08-20 16:10:02 +0200

    getpid: use tos

--- sys/src/libc/9sys/getpid.c
+++ sys/src/libc/9sys/getpid.c
@@ -1,17 +1,9 @@
 #include	<u.h>
 #include	<libc.h>
+#include	<tos.h>
 
 int
 getpid(void)
 {
-	char b[20];
-	int f;
-
-	memset(b, 0, sizeof(b));
-	f = open("#c/pid", 0);
-	if(f >= 0) {
-		read(f, b, sizeof(b));
-		close(f);
-	}
-	return atol(b);
+	return _tos->pid;
 }
