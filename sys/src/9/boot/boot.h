95742d95a94a095baae9ca713c8b05438786d7e6
cinap_lenrek <cinap_lenrek@centraldogma>
2011-12-13 16:16:21 +0100

    cleanup

--- sys/src/9/boot/boot.h
+++ sys/src/9/boot/boot.h
@@ -4,8 +4,8 @@ enum {
 
 extern char*	bootdisk;
 
-extern void fatal(char*);
+extern void	fatal(char*);
 extern int	readfile(char*, char*, int);
-extern void run(char*, ...);
+extern void	run(char*, ...);
 extern void	setenv(char*, char*, int);
 extern int	writefile(char*, char*, int);
