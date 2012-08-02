a13a30e854d22639dc34b5502b402d8a579966e6
cinap_lenrek <cinap_lenrek@gmx.de>
2012-08-02 22:20:55 +0200

    ndb: recognize ip6.arpa postfix in mkptrname()

--- sys/src/libndb/dnsquery.c
+++ sys/src/libndb/dnsquery.c
@@ -81,7 +81,7 @@ mkptrname(char *ip, char *rip, int rlen)
 	char *p, *np;
 	int len;
 
-	if(cistrstr(ip, "in-addr.arpa")){
+	if(cistrstr(ip, "in-addr.arpa") || cistrstr(ip, "ip6.arpa")){
 		nstrcpy(rip, ip, rlen);
 		return;
 	}
