7f22b32e6359bb1381fc2336b9ec876036eb72a6
cinap_lenrek <cinap_lenrek@gmx.de>
2012-09-01 14:12:29 +0200

    isodate in libc? revert every commit for great justice!

--- sys/src/cmd/date.c
+++ sys/src/cmd/date.c
@@ -1,19 +1,17 @@
 #include <u.h>
 #include <libc.h>
 
-int uflg, nflg, iflg, tflg;
+int uflg, nflg;
 
 void
 main(int argc, char *argv[])
 {
 	ulong now;
-	Tm *tm;
+
 	ARGBEGIN{
 	case 'n':	nflg = 1; break;
 	case 'u':	uflg = 1; break;
-	case 't':	tflg = 1; /* implies -i */
-	case 'i':	iflg = 1; break;
-	default:	fprint(2, "usage: date [-itun] [seconds]\n"); exits("usage");
+	default:	fprint(2, "usage: date [-un] [seconds]\n"); exits("usage");
 	}ARGEND
 
 	if(argc == 1)
@@ -23,17 +21,10 @@ main(int argc, char *argv[])
 
 	if(nflg)
 		print("%ld\n", now);
-	else if(iflg) {
-		tm = uflg ? gmtime(now) : localtime(now);
-		if(tflg)
-			print("%s\n", isotime(tm));
-		else
-			print("%s\n", isodate(tm));
-	} else {
-		if(uflg)
-			print("%s", asctime(gmtime(now)));
-		else
-			print("%s", ctime(now));
-	}
+	else if(uflg)
+		print("%s", asctime(gmtime(now)));
+	else
+		print("%s", ctime(now));
+	
 	exits(0);
 }
