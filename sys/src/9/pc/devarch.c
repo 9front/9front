49c9955aea7348027e3e6925609904ee11dfdee6
cinap_lenrek <cinap_lenrek@gmx.de>
2013-06-21 02:49:08 +0200

    reverting *notsc= option, this needs another revision

--- sys/src/9/pc/devarch.c
+++ sys/src/9/pc/devarch.c
@@ -797,7 +797,7 @@ cpuidentify(void)
 	/*
 	 *  if there is one, set tsc to a known value
 	 */
-	if((m->cpuiddx & Tsc) != 0 && getconf("*notsc") == nil){
+	if(m->cpuiddx & Tsc){
 		m->havetsc = 1;
 		cycles = _cycles;
 		if(m->cpuiddx & Cpumsr)
