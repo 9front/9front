0542f08d10334abfe92d23484379563bb6f2b31e
cinap_lenrek <cinap_lenrek@gmx.de>
2013-06-29 12:10:12 +0200

    dump9660: fix volume space size field (from sources, paure)
    http://www.ecma-international.org/publications/files/ECMA-ST/Ecma-119.pdf
    
    page 19:
    
    Volume Space Size (BP 81 to 88)
    This field shall specify as a 32-bit number the number of Logical Blocks
    in which the Volume Space of the volume is recorded.
    This field shall be recorded according to 7.3.3.

--- sys/src/cmd/disk/9660/dump9660.c
+++ sys/src/cmd/disk/9660/dump9660.c
@@ -307,11 +307,10 @@ Dofix:
 		 * Patch in root directories.
 		 */
 		setroot(cd, cd->iso9660pvd, iroot.block, iroot.length);
-		setvolsize(cd, cd->iso9660pvd, (vlong)cd->nextblock * Blocksize);
+		setvolsize(cd, cd->iso9660pvd, cd->nextblock);
 		if(cd->flags & CDjoliet){
 			setroot(cd, cd->jolietsvd, jroot.block, jroot.length);
-			setvolsize(cd, cd->jolietsvd,
-				(vlong)cd->nextblock * Blocksize);
+			setvolsize(cd, cd->jolietsvd, cd->nextblock);
 		}
 	}else{
 		/*
@@ -341,11 +340,10 @@ Dofix:
 		 * Patch in new root directory entry.
 		 */
 		setroot(cd, cd->iso9660pvd, idumproot.block, idumproot.length);
-		setvolsize(cd, cd->iso9660pvd, (vlong)cd->nextblock * Blocksize);
+		setvolsize(cd, cd->iso9660pvd, cd->nextblock);
 		if(cd->flags & CDjoliet){
 			setroot(cd, cd->jolietsvd, jdumproot.block, jdumproot.length);
-			setvolsize(cd, cd->jolietsvd,
-				(vlong)cd->nextblock * Blocksize);
+			setvolsize(cd, cd->jolietsvd, cd->nextblock);
 		}
 	}
 	writepathtables(cd);	
