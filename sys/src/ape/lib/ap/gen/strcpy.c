43ae5531953f65860c5c6e72ac5218aa3a235bbf
cinap_lenrek <cinap_lenrek@felloff.net>
2014-01-20 00:05:19 +0100

    ape: remove local copy of memccpy()

--- sys/src/ape/lib/ap/gen/strcpy.c
+++ sys/src/ape/lib/ap/gen/strcpy.c
@@ -1,21 +1,6 @@
 #include <string.h>
 #define	N	10000
 
-static void*
-memccpy(void *a1, void *a2, int c, unsigned long n)
-{
-	char *s1, *s2;
-
-	s1 = a1;
-	s2 = a2;
-	while(n > 0) {
-		if((*s1++ = *s2++) == c)
-			return s1;
-		n--;
-	}
-	return 0;
-}
-
 char*
 strcpy(char *s1, const char *s2)
 {
