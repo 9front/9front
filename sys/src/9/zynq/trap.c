2ef69f42d8372d0efc046e590191d73573b545fa
cinap_lenrek <cinap_lenrek@felloff.net>
2014-12-24 11:20:40 +0100

    zynq: fix kernel configuration so it builds

--- sys/src/9/zynq/trap.c
+++ sys/src/9/zynq/trap.c
@@ -203,7 +203,7 @@ syscall(Ureg *ureg)
 			syscallfmt(scallnr, ureg->pc, (va_list) up->s.args);
 			s = splhi();
 			up->procctl = Proc_stopme;
-			procctl(up);
+			procctl();
 			splx(s);
 			startns = todget(nil);
 		}
@@ -234,7 +234,7 @@ syscall(Ureg *ureg)
 		sysretfmt(scallnr, (va_list) up->s.args, ret, startns, stopns);
 		s = splhi();
 		up->procctl = Proc_stopme;
-		procctl(up);
+		procctl();
 		splx(s);
 	}
 	
@@ -261,7 +261,7 @@ notify(Ureg *ureg)
 	Note *n;
 
 	if(up->procctl)
-		procctl(up);
+		procctl();
 	if(up->nnote == 0)
 		return 0;
 
