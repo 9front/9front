433b18ec2b54387119abdeb0a85767cd92e4a23b
cinap_lenrek <cinap_lenrek@felloff.net>
2015-04-16 14:32:42 +0200

    cc: catch non constant pointer initializers
    char	FOO[] = "abc";	/* ok */
    char	*BAR = FOO;		/* ok */
    char	*BAZ = BAR;		/* wrong */

--- sys/src/cmd/cc/dcl.c
+++ sys/src/cmd/cc/dcl.c
@@ -381,8 +381,14 @@ init1(Sym *s, Type *t, long o, int exflag)
 				diag(a, "initialization of incompatible pointers: %s\n%T and %T",
 					s->name, t, a->type);
 			}
-			if(a->op == OADDR)
+			if(a->op == OADDR) {
 				a = a->left;
+				goto gext;
+			}
+			if(a->type->etype == TIND) {
+				diag(a, "initializer is not a constant: %s", s->name);
+				return Z;
+			}
 			goto gext;
 		}
 
