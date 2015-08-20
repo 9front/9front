78c45541eb66a661f4e45c2f86bb5ac37de2408b
cinap_lenrek <cinap_lenrek@felloff.net>
2015-08-20 15:47:49 +0200

    libauthsrv: add missing files (thanks mischief)

---
+++ sys/src/libauthsrv/_asgetresp.c
@@ -0,0 +1,29 @@
+#include <u.h>
+#include <libc.h>
+#include <authsrv.h>
+
+int
+_asgetresp(int fd, Ticket *t, Authenticator *a, Authkey *k)
+{
+	char tbuf[TICKETLEN+AUTHENTLEN];
+	int n, m;
+
+	memset(t, 0, sizeof(Ticket));
+	if(a != nil)
+		memset(a, 0, sizeof(Authenticator));
+
+	n = _asrdresp(fd, tbuf, sizeof(tbuf));
+	if(n <= 0)
+		return -1;
+
+	m = convM2T(tbuf, n, t, k);
+	if(m <= 0)
+		return -1;
+
+	if(a != nil){
+		if(convM2A(tbuf+m, n-m, a, t) <= 0)
+			return -1;
+	}
+
+	return 0;
+}
