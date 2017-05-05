f317d233f46bdc85c02c0a991ac2a4788ad34fdb
cinap_lenrek <cinap_lenrek@felloff.net>
2017-05-06 00:49:47 +0200

    devproc: can't wait for ourselfs to stop (thanks Shamar)

--- sys/src/9/port/devproc.c
+++ sys/src/9/port/devproc.c
@@ -1212,6 +1212,8 @@ procstopwait(Proc *p, int ctl)
 		error(Eprocdied);
 	if(ctl != 0)
 		p->procctl = ctl;
+	if(p == up)
+		return;
 	p->pdbg = up;
 	qunlock(&p->debug);
 	up->psstate = "Stopwait";
