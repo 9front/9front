d68417bc01ef1258e707d95d4785c189fcad5834
aiju <devnull@localhost>
2018-03-18 03:01:21 +0000

    satmore: don't try to override user defined variables

--- sys/src/libsat/satmore.c
+++ sys/src/libsat/satmore.c
@@ -11,11 +11,13 @@ satmore(SATSolve *s)
 	if(s == nil) return 1;
 	s->scrap = a = satrealloc(s, nil, s->nvar * sizeof(int));
 	n = 0;
-	for(i = 0; i < s->nvar; i++)
+	for(i = 0; i < s->nvar; i++){
+		if((s->var[i].flags & VARUSER) != 0) continue;
 		switch(s->lit[2*i].val){
 		case 0: a[n++] = i+1; break;
 		case 1: a[n++] = -(i+1); break;
 		}
+	}
 	if(n > 0)
 		satadd1(s, a, n);
 	free(a);
