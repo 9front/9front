930efe67e81635e632f1e6f93e1c56499c79a55f
cinap_lenrek <cinap_lenrek@felloff.net>
2018-05-20 23:22:41 +0200

    ape: fix auth.h

--- sys/include/ape/auth.h
+++ sys/include/ape/auth.h
@@ -63,6 +63,7 @@ struct AuthInfo
 struct Chalstate
 {
 	char	*user;
+	char	*dom;
 	char	chal[MAXCHLEN];
 	int	nchal;
 	void	*resp;
@@ -81,7 +82,7 @@ struct	Chapreply		/* for protocol "chap" */
 	char	resp[MD5LEN];
 };
 
-struct	MSchapreply	/* for protocol "mschap" */
+struct	MSchapreply		/* for protocol "mschap" and "ntlm" */
 {
 	char	LMresp[24];		/* Lan Manager response */
 	char	NTresp[24];		/* NT response */
