d4e89fe76a24ab08aae50f0918cf3e65efbc779b
cinap_lenrek <cinap_lenrek@felloff.net>
2022-03-14 11:16:11 +0000

    ape: fix wrong write size for _assert()

--- sys/src/ape/lib/ap/gen/_assert.c
+++ sys/src/ape/lib/ap/gen/_assert.c
@@ -9,9 +9,9 @@ _assert(char *f, unsigned line)
 	write(2, "assertion failed: file ", 23);
 	for(p = f; *p; p++) continue;
 	write(2, f, p-f);
-	write(2, ":", 7);
 	*--s = '\n';
 	do *--s = line%10 + '0'; while (line /= 10);
+	*--s = ':';
 	write(2, s, &buf[20] - s);
 	abort();
 	
