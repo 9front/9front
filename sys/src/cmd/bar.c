c5bfb20546c1694803b0a5d9b7477e5c27af0dba
qwx <qwx@sciops.net>
2022-10-23 20:00:24 +0000

    bar: fix item boundaries misspacing

--- sys/src/cmd/bar.c
+++ sys/src/cmd/bar.c
@@ -147,6 +147,7 @@ redraw(void)
 	draw(screen, r, cback, nil, ZP);
 	string(screen, p, ctext, ZP, f, s);
 	if(hlitem >= 0){
+		r.min.x += sepw;
 		for(i = 0; i < hlitem; i++)
 			r.min.x += itemw[i];
 		r.max.x = r.min.x + itemw[i];
@@ -226,7 +227,7 @@ clicked(int x, int buttons)
 		return;
 	}
 
-	for(i = ix = 0; i < nitems; i++){
+	for(i = 0, ix = sepw; i < nitems; i++){
 		ix += itemw[i];
 		if(x <= ix){
 			fprint(1, "%d\t%s\n", buttons, items[i]);
