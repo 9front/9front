389adc25764a27ff5feffabd185573e64ecbebed
Ori Bernstein <ori@eigenstate.org>
2025-11-28 15:50:40 +0000

    gefs: only print 'stalled epoch' once after 500 seconds

--- sys/src/cmd/gefs/blk.c
+++ sys/src/cmd/gefs/blk.c
@@ -884,9 +884,7 @@ Again:
 	for(i = 0; i < agetl(&fs->nworker); i++){
 		e = agetl(&fs->lepoch[i]);
 		if((e & Eactive) && e != (ge | Eactive)){
-			if(delay < 1000)
-				delay++;
-			else
+			if(delay == 1000)
 				fprint(2, "stalled epoch %lx [worker %d]\n", e, i);
 			sleep(delay);
 			goto Again;
