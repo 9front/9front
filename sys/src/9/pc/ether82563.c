285ccfbd07badd61530f25dedf842893550eb51f
sl <sl@k>
2025-12-17 03:04:25 +0000

    sys/src/9/pc/ether82563.c: add did 0d4e to i219-lm (makes thinkpad l13 gen 1 core i7 vpro ethernet work)

--- sys/src/9/pc/ether82563.c
+++ sys/src/9/pc/ether82563.c
@@ -2027,6 +2027,7 @@ didtype(int d)
 	case 0x0d4c:		/* i219-lm */
 	case 0x15bc:		/* i219-v */
 	case 0x0d4f:		/* i219-v */
+	case 0x0d4e:		/* i219-lm */
 		return i219;
 	case 0x151f:		/* i350 “powerville” eeprom-less */
 	case 0x1521:		/* i350 copper */
