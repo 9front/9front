87ec4550f392180df2ccd3fc48b3ca91f3bcd09c
Jacob Moody <moody@posixcafe.org>
2026-01-26 00:18:08 +0000

    libc: simplify utf* functions
    utfutf() is no longer needed.

--- sys/src/libc/port/utfrrune.c
+++ sys/src/libc/port/utfrrune.c
@@ -4,27 +4,20 @@
 char*
 utfrrune(char *s, long c)
 {
-	long c1;
 	Rune r;
-	char *s1;
+	int n;
+	char *p;
+	char buf[UTFmax + 1] = {0};
 
 	if(c < Runesync)		/* not part of utf sequence */
 		return strrchr(s, c);
 
-	s1 = 0;
-	for(;;) {
-		c1 = *(uchar*)s;
-		if(c1 < Runeself) {	/* one byte rune */
-			if(c1 == 0)
-				return s1;
-			if(c1 == c)
-				s1 = s;
-			s++;
-			continue;
-		}
-		c1 = chartorune(&r, s);
-		if(r == c)
-			s1 = s;
-		s += c1;
+	r = c;
+	n = runetochar(buf, &r);
+	p = nil;
+	while(s = strstr(s, buf)){
+		p = s;
+		s += n;
 	}
+	return p;
 }
--- sys/src/libc/port/utfrune.c
+++ sys/src/libc/port/utfrune.c
@@ -4,26 +4,13 @@
 char*
 utfrune(char *s, long c)
 {
-	long c1;
 	Rune r;
-	int n;
+	char buf[UTFmax + 1] = {0};
 
 	if(c < Runesync)		/* not part of utf sequence */
 		return strchr(s, c);
 
-	for(;;) {
-		c1 = *(uchar*)s;
-		if(c1 < Runeself) {	/* one byte rune */
-			if(c1 == 0)
-				return 0;
-			if(c1 == c)
-				return s;
-			s++;
-			continue;
-		}
-		n = chartorune(&r, s);
-		if(r == c)
-			return s;
-		s += n;
-	}
+	r = c;
+	runetochar(buf, &r);
+	return strstr(s, buf);
 }
