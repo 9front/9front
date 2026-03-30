cfde707b706eb92af05ec5f4395a0c109a0b6cd2
cinap_lenrek <cinap_lenrek@felloff.net>
2026-03-30 21:39:06 +0000

    ether82557: Add PCI ID for built-in LAN adapter on Fujitsu-Siements Amilo Si 1520 laptops. (thanks Yaroslav)

--- sys/src/9/pc/ether82557.c
+++ sys/src/9/pc/ether82557.c
@@ -945,6 +945,7 @@ i82557pci(void)
 		case 0x103A:		/* Intel 82562 PRO/100 VE */
 		case 0x103D:		/* Intel 82562 PRO/100 VE */
 		case 0x1064:		/* Intel 82562 PRO/100 VE */
+		case 0x1092:		/* Intel 82562 PRO/100 VE */
 		case 0x2449:		/* Intel 82562ET */
 		case 0x27DC:		/* Intel 82801G PRO/100 VE */
 			nop = 1;
