88f079670235fd92e09c37e10fd80ed62e9800fe
cinap_lenrek <cinap_lenrek@felloff.net>
2026-04-29 10:13:08 +0000

    ether82557: add PCIID for Intel 82801CAM (thanks yarikos)
    "Representative hardware includes Compaq Evo N610c laptop."

--- sys/src/9/pc/ether82557.c
+++ sys/src/9/pc/ether82557.c
@@ -941,6 +941,7 @@ i82557pci(void)
 		case 0x103B:		/* Intel 82562EM */
 		case 0x103C:		/* Intel 82562EM */
 		case 0x1050:		/* Intel 82562EZ */
+		case 0x1038:		/* Intel 82801CAM */
 		case 0x1039:		/* Intel 82801BD PRO/100 VE */
 		case 0x103A:		/* Intel 82562 PRO/100 VE */
 		case 0x103D:		/* Intel 82562 PRO/100 VE */
