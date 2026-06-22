2c45fce68d17d7887a50b4d6cd2d794984d6c816
Ori Bernstein <ori@eigenstate.org>
2026-06-22 03:21:34 +0000

    upas/smtpd: when parsing PLAIN auth, only look for nul in bounds.

--- sys/src/cmd/upas/smtp/smtpd.c
+++ sys/src/cmd/upas/smtp/smtpd.c
@@ -1655,7 +1655,7 @@ passauth(char *u, char *secret)
 void
 auth(String *mech, String *resp)
 {
-	char *user, *pass;
+	char *user, *pass, *p, *e;
 	AuthInfo *ai = nil;
 	Chalstate *chs = nil;
 	String *s_resp1_64 = nil, *s_resp2_64 = nil, *s_resp1 = nil;
@@ -1694,8 +1694,21 @@ auth(String *mech, String *resp)
 			goto bomb_out;
 		}
 		memset(s_to_c(s_resp1_64), 'X', s_len(s_resp1_64));
-		user = s_to_c(s_resp1) + strlen(s_to_c(s_resp1)) + 1;
-		pass = user + strlen(user) + 1;
+		p = s_to_c(s_resp1);
+		e = p + s_len(s_resp1);
+		/* authorization-id\0user-id\0password */
+		if((p = memchr(p, 0, (e - p))) == nil){
+			rejectcount++;
+			reply("501 5.5.4 Cannot decode base64\r\n");
+			goto bomb_out;
+		}
+		user = ++p;
+		if((p = memchr(p, 0, (e - p))) == nil){
+			rejectcount++;
+			reply("501 5.5.4 Cannot decode base64\r\n");
+			goto bomb_out;
+		}
+		pass = ++p;
 		authenticated = passauth(user, pass) != -1;
 		memset(pass, 'X', strlen(pass));
 		goto windup;
